ad696c5ae82296549dd7afa07494a9a1d39f4c05
Ashish Bhosle <asbhosle2003@gmail.com>
2024-06-26 19:11:02 +0530

    git push

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import ModalProvider from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -30,7 +31,7 @@ export default function RootLayout({
         enableSystem={false}
         storageKey="discord-theme"
         >
-        
+        <ModalProvider />
           {children}
        </ThemeProvider>
         
