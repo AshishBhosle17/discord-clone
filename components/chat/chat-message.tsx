1d2a5baea8f85f760c18861fd4ced1bfdaa0da1e
Ashish Bhosle <asbhosle2003@gmail.com>
2024-07-02 10:57:08 +0530

    Chat socket hook

--- components/chat/chat-message.tsx
+++ components/chat/chat-message.tsx
@@ -3,7 +3,7 @@
 import { Member, Message, Profile } from "@prisma/client";
 import React, { ElementRef, Fragment, useRef } from "react";
 import ChatWelcome from "./chat-welcome";
-
+import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import ChatItem from "./chat-item";
 
@@ -50,7 +50,27 @@ const ChatMessages = ({
     }
   }
 
-  
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+  });
+
+  useChatSocket({ queryKey, addKey, updateKey });
+  useChatScroll({
+    chatRef,
+    bottomRef,
+    loadMore: fetchNextPage,
+    shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+    count: data?.pages?.[0]?.items?.length ?? 0,
+  })
 
   if (status === "pending") {
     return (
@@ -76,22 +96,53 @@ const ChatMessages = ({
   }
   return (
     <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
-    
-
-
-      
+     {!hasNextPage && <div className="flex-1" />}
+      {!hasNextPage && (
+        <ChatWelcome
+          type={type}
+          name={name}
+        />
+      )}
+      {hasNextPage && (
+        <div className="flex justify-center">
+          {isFetchingNextPage ? (
+            <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4" />
+          ) : (
+            <button
+              onClick={() => fetchNextPage()}
+              className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition"
+            >
+              Load previous messages
+            </button>
+          )}
+        </div>
+      )}
       <div className="flex flex-col-reverse mt-auto">
-        
-          
-            <Fragment>
+        {
+          data?.pages.map((group,i)=>(
+            <Fragment key={i}>
               {
-                 
+                group.items.map((message:MessageWithMemberWithProfile)=>(
+                  <ChatItem
+                  key={message.id}
+                  id={message.id}
+                  currentMember={member}
+                  member={message.member}
+                  content={message.content}
+                  fileUrl={message.fileUrl}
+                  deleted={message.deleted}
+                  timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                  isUpdated={message.updatedAt !== message.createdAt}
+                  socketUrl={socketUrl}
+                  socketQuery={socketQuery}
+                />
+                ))
               }
 
 
             </Fragment>
-          
-        
+          ))
+        }
         </div>
 
         <div ref={bottomRef} />
