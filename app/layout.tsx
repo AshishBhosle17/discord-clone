52719821489f1f93227601cf5576170c36e3e303
Ashish Bhosle <asbhosle2003@gmail.com>
2024-07-02 19:52:41 +0530

    Query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 import ModalProvider from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
+import QueryProvider from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -20,25 +22,21 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-       <html lang="en" suppressHydrationWarning>
-       <body className={cn(
-          font.className,
-          "bg-white dark:bg-[#313338]"
-        )}>
-      
-        <ThemeProvider attribute="class"
-        defaultTheme="dark"
-        enableSystem={false}
-        storageKey="discord-theme"
-        >
-        <ModalProvider />
-          {children}
-       </ThemeProvider>
-        
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            storageKey="discord-theme"
+          >
+            <SocketProvider>
+              <ModalProvider />
+              <QueryProvider> {children} </QueryProvider>
+            </SocketProvider>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  
-   
   );
 }
